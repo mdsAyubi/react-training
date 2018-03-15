763669e5277e31267b180f370475813dff4f0764
alilog <alilogmaj@gmail.com>
2018-03-15 08:14:12 +0530

    ReactSeed
    Helping build react

---
+++ distribute/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class App extends Component {
+    render() {
+      return (
+        <div>Hellooooo ReaaaaaaaaaaCt</div>
+      );
+    }
+  }
+
+ReactDOM.render(<App/>, document.querySelector(".container"));
